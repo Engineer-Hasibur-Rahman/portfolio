80ba5254e8ca231b1181044f16d776a62ef6c8b0
Engineer-Hasibur-Rahman <hasibur2060@gmail.com>
2025-09-06 02:05:15 +0600

    setup project

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="relative bg-black">
+       <div>
+        <h1>Hello! Portfolio</h1>
+        <p>I'm Hasibur Rahman, a passionate developer specializing in building web applications.</p>
+        <p>Welcome to my portfolio where I showcase my projects and skills.</p>
+       </div>
+      </main>
+
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+      </footer>
+    </div>
+  );
+}
