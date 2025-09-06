ddd3b23e7590d8856944ded21506f225e91985b4
Engineer-Hasibur-Rahman <hasibur2060@gmail.com>
2025-09-06 14:35:42 +0600

    Added: Sentry api

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>
-      <main className="relative
-       bg-black flex justify-center 
-       items-center flex-col overflow-hidden 
-       mx-auto sm:px-10 px-5">
-       <div className="max-w-7xl w-full">
-       <Hero/>
-       </div>
-      </main>
-
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-
-      </footer>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
